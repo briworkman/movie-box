c7c912ddc2cd336d9b1374b7728c857b40daf771
Brianna Workman <brianna.workman@hotmail.com>
2019-11-12 10:59:28 -0600

    Sets up state, fetches data from API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  API_URL,
+  API_KEY,
+  API_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../config";
 
 // import Components
 import HeroImage from "./elements/HeroImage";
@@ -8,7 +15,35 @@ import MovieThumb from "./elements/MovieThumb";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
-const Home = () => (
+const Home = () => {
+  const [state, setState] = useState({movies: []});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  console.log(state);
+
+  const fetchMovies = async endpoint => {
+    setError(false);
+    loading(true);
+
+    try {
+      const response = await (await fetch(endpoint)).json();
+      setState(state => ({
+        ...state,
+        movies: [...response.results],
+        heroImage: state.heroImage || response.results[0],
+        currentPage: response.page,
+        totalPages: response.total_pages
+      }));
+
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+    setLoading(false);
+  }
+
+  return(
   <>
     <HeroImage />
     <SearchBar />
@@ -16,7 +51,7 @@ const Home = () => (
     <MovieThumb />
     <LoadMoreBtn />
     <Spinner />
-  </>
-);
+  </>;
+  )};
 
 export default Home;
